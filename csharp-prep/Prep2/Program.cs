e349913beaa0464c26127868dfcd4a27d87cbcc8
JacobMo28 <mo24001@byui.edu>
2026-09-22 09:57:20 -0600

    Prep1 and Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //int myNumber = int.Parse(numberString) to convert
+
+
+        string grade;
+        string letter;
+
+
+        Console.Write("Input Grade Percentage (Ex: 95): ");
+        grade = Console.ReadLine();
+        int intGrade = int.Parse(grade);
+
+
+        if (intGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (intGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (intGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (intGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (intGrade >= 70)
+        {
+            Console.WriteLine("You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course. Try again next time.");
+        }
+
+
+        
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello there!"); 
+        int x = 10;
+        int y = 30;
+        int z = 40;
+
+        if (x == 10 && y == 30 || z == 30)
+        {
+            Console.WriteLine("X is 10");
+            Console.WriteLine("Y is 30");
+        }
+        else if (x == 20)
+        {
+            Console.WriteLine("X is 20");
+        }
+        else
+        {
+            Console.WriteLine("Default Output");
+        }
     }
 }
+
